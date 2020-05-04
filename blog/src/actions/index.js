640af45f5972b34bf4ffbacf873c131e9c01ac5b
Pseudo Geek <pseudogeek7@gmail.com>
2020-05-04 05:52:20 +0500

    simple blog app - fetchs dummy posts from ext API using react, redux-thunk

---
+++ blog/src/actions/index.js
@@ -0,0 +1,23 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+// without thunk throws error
+// export const fetchPosts = async ()=>{
+//     const response = await jsonPlaceholder.get('/posts');
+//     return {
+//         type: 'FETCH_POSTS',
+//         payload: response
+//     };
+// };
+
+// without arrow function using to fetchposts
+// export const fetchPosts = ()=> {
+//     return async function (dispatch, getState){
+//         const response = await jsonPlaceholder.get('/posts');
+//         dispatch({type:'FETCH_POSTS', payload: response});
+//     };
+// };
+
+//refeactored code for simplicity
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: response });
+};
