5c4820067867ea9254221a61436360dbaf1be21a
Pseudo Geek <pseudogeek7@gmail.com>
2020-05-10 08:27:57 +0500

    add nav into new created components

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,26 +1,27 @@
-import React from  'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route} from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
-const PageOne = ()=> {
-    return <div>Page One
-        <Link to="/pagetwo">Nav to Pg 2</Link>
-    </div>;
-}
-
-const PageTwo = ()=>{
-    return (<div> Page Two
-        <Link to="/">Nav to Pg 1</Link>
-    </div>);
-}
 const App = () => {
-    return (<div>
-        <BrowserRouter>
+  return (
+    <div className="ui container">
+      <BrowserRouter>
         <div>
-            <Route path="/" exact component={PageOne}/>
-            <Route path="/pagetwo" component={PageTwo}/>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
-        </BrowserRouter>
-    </div>);
-}
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
